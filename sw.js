2d4625ca3097c7087a1f2a07941977c1258e5175
Matheus Vinicius Gabriel <matheusvinic55@gmail.com>
2026-07-15 00:19:18 -0300

    Atualiza cache do camarim interativo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sucupira-v15';
+const CACHE='sucupira-v16';
 const CORE=['/','/index.html','/manifest.webmanifest','/assets/capa-sucupira.webp','/assets/personagens/zeca-diabo.svg','/assets/personagens/dr-juarez.svg','/assets/personagens/telma.svg','/assets/personagens/donana.svg','/assets/personagens/gisa.svg','/assets/personagens/ceceu.svg','/assets/personagens/zora.svg','/icons/icon-192.png','/icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
