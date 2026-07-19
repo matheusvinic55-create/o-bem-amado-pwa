d213a7988aded3c98b44b7ef310d697a918a674c
Matheus Vinicius Gabriel <matheusvinic55@gmail.com>
2026-07-18 22:18:51 -0300

    chore: renovar cache da casa de buzios

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sucupira-v35';
+const CACHE='sucupira-v36';
 const CORE=['/','/index.html','/buzios','/buzios/','/buzios/index.html','/manifest.webmanifest','/assets/capa-sucupira.webp','/assets/personagens/zeca-diabo.svg','/assets/personagens/dr-juarez.svg','/assets/personagens/telma.svg','/assets/personagens/donana.svg','/assets/personagens/gisa.svg','/assets/personagens/ceceu.svg','/assets/personagens/zora.svg','/icons/icon-192.png','/icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
